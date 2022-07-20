97890e628b3cf6cabd054706ee15c298c14c18e0
simo <mohamed.hanini95@gmail.com>
2022-07-20 12:56:28 +0100

    FrontEnd [ create auth store  ] => DONE :)

---
+++ src/store/auth.js
@@ -0,0 +1,92 @@
+export default {
+    state:{
+        loggedIn:false,
+        user: null,
+        token: null
+    },
+    mutations:{
+        SET_token(state,payload){
+            state.token = payload
+        },
+        SET_user(state,payload){
+            state.user = payload
+        },
+        SET_loggedIn(state,payload){
+            state.loggedIn = payload
+        }
+    },
+    actions:{
+        performLoginAction({commit}, payload){
+            return new Promise( (resolve, reject) => {
+                axios.post('https://laravel-vuejs-jwt-vuetify.herokuapp.com/api/auth/login',{
+                    email:payload.email,
+                    password:payload.password
+                    }).then(res =>{
+                        commit('SET_token', res.data.access_token)
+                        commit('SET_user', res.data.user)
+                        commit('SET_loggedIn', true)
+                        resolve(res)
+                    }
+                    ).catch( err => {
+                        reject(err)
+                    })
+            })
+        },
+        performRegisterAction({commit}, payload){
+            return new Promise( (resolve, reject) => {
+                axios.post('https://laravel-vuejs-jwt-vuetify.herokuapp.com/api/auth/register',{
+                    name:payload.name,
+                    email:payload.email,
+                    password:payload.password
+                    }).then(res =>{
+                        commit('SET_token', res.data.access_token)
+                        commit('SET_user', res.data.user)
+                        commit('SET_loggedIn', true)
+                        resolve(res)
+                    }
+                    ).catch( err => {
+                        reject(err)
+                    })
+            })
+        },
+        performLogoutAction({state,commit}){
+            return new Promise( (resolve, reject) => {
+                axios.post('https://laravel-vuejs-jwt-vuetify.herokuapp.com/api/auth/logout',{
+                    token:state.token                    
+                    }).then(res =>{
+                        commit('SET_token', null)
+                        commit('SET_user', null)
+                        commit('SET_loggedIn', false)
+                        resolve(res)
+                    }
+                    ).catch( err => {
+                        reject(err)
+                    })
+            })
+        },
+        UpdateUserProfile({commit,state}, payload){
+            return new Promise( (resolve, reject) => {
+                axios
+                .patch('https://laravel-vuejs-jwt-vuetify.herokuapp.com/api/auth/update',{
+                    name:payload.name,
+                    email:payload.email,
+                    token:state.token
+                    }).then(res =>{
+                        commit('SET_user', res.data.user)
+                        resolve(res)
+                    }
+                    ).catch( err => {
+                        reject(err)
+                    })
+            })
+        }
+    },
+    getters:{
+        get_loggedIn(state){
+            return state.loggedIn
+        },
+        get_user(state){
+            return state.user
+        }
+    }
+}
